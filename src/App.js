f037a290ece756ed0a8fa2853ef82770fbcf4ca5
Narek <nareksargsyan17>
2023-07-17 12:32:25 +0400

    imported artd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {LaptopOutlined, NotificationOutlined, UserOutlined} from '@ant-design/icons';
+import {Breadcrumb, Layout, Menu, theme} from 'antd';
+import { HeaderBar } from "./Components/HeaderBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const {Content, Footer, Sider} = Layout;;
 
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+    const key = String(index + 1);
+    return {
+        key: `sub${key}`,
+        icon: React.createElement(icon),
+        label: `subnav ${key}`,
+        children: new Array(4).fill(null).map((_, j) => {
+            const subKey = index * 4 + j + 1;
+            return {
+                key: subKey,
+                label: `option${subKey}`,
+            };
+        }),
+    };
+});
+const App = () => {
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+    return (
+        <Layout>
+            <HeaderBar />
+            <Content
+                style={{
+                    padding: '0 50px',
+                }}
+            >
+                {/*<Breadcrumb*/}
+                {/*    style={{*/}
+                {/*        margin: '16px 0',*/}
+                {/*    }}*/}
+                {/*>*/}
+                {/*    <Breadcrumb.Item>Home</Breadcrumb.Item>*/}
+                {/*    <Breadcrumb.Item>List</Breadcrumb.Item>*/}
+                {/*    <Breadcrumb.Item>App</Breadcrumb.Item>*/}
+                {/*</Breadcrumb>*/}
+                <Layout
+                    style={{
+                        padding: '24px 0',
+                        height: "100vh",
+                        background: colorBgContainer,
+                    }}
+                >
+                    <Sider
+                        style={{
+                            background: colorBgContainer,
+                        }}
+                        width={200}
+                    >
+                        <Menu
+                            mode="inline"
+                            defaultOpenKeys={['sub1']}
+                            style={{
+                                height: '100%',
+                            }}
+                            items={items2}
+                        />
+                    </Sider>
+                    <Content
+                        style={{
+                            padding: '0 24px',
+                            minHeight: 280,
+                        }}
+                    >
+                        Content
+                    </Content>
+                </Layout>
+            </Content>
+            <Footer
+                style={{
+                    textAlign: 'center',
+                }}
+            >
+                Ant Design ©2023 Created by Ant UED
+            </Footer>
+        </Layout>
+    );
+};
 export default App;
---
+++ src/Components/HeaderBar.js
@@ -0,0 +1,14 @@
+import {Header} from "antd/es/layout/layout";
+
+export function HeaderBar() {
+    return (
+        <Header
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+            }}
+        >
+
+        </Header>
+    )
+}
